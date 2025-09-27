9921fd3f7926d8ed08c8b454e558e9c35d8a2d1b
koudenpa <koudenpa@hatena.ne.jp>
2025-09-27 22:01:35 +0900

    chore: relax lint rules for worker stack

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,28 +1,27 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier'
   ],
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: 'module',
-    project: './tsconfig.json'
+    sourceType: 'module'
   },
   env: {
     node: true,
     es2022: true
   },
   rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'no-console': 'warn'
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'off',
+    'prefer-const': 'off'
   },
   ignorePatterns: ['dist', 'node_modules', '*.js']
 };

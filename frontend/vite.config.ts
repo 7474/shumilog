c4c2db46a2cd3cbb9215d1c824fdfa75f1cec36a
koudenpa <koudenpa@hatena.ne.jp>
2025-09-28 09:01:17 +0900

    適当にデザインしてもらう

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types="vitest" />
+/// <refe types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -6,6 +6,11 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       '/api': {
